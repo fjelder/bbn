26a01e850fc052655c0d778bf22bced097c67d76
fjelder <42210053+fjelder@users.noreply.github.com>
2023-03-13 22:14:52 +0100

    work 130322

--- src/js/test.js
+++ src/js/test.js
@@ -20,48 +20,59 @@ function checkDarkMode() {
   }
 }
 function darkMode() {
-  var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
-  var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-
+  var themeToggleDarkIcon = document.getElementsByClassName(
+    "theme-toggle-dark-icon"
+  );
+  var themeToggleLightIcon = document.getElementsByClassName(
+    "theme-toggle-light-icon"
+  );
   // Change the icons inside the button based on previous settings
   if (
     localStorage.getItem("color-theme") === "dark" ||
     (!("color-theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
-    themeToggleLightIcon.classList.remove("hidden");
+    for (let i = 0; i < themeToggleLightIcon.length; i++) {
+      themeToggleLightIcon[i].classList.remove("hidden");
+    }
   } else {
-    themeToggleDarkIcon.classList.remove("hidden");
+    for (let i = 0; i < themeToggleDarkIcon.length; i++) {
+      themeToggleDarkIcon[i].classList.remove("hidden");
+    }
   }
 
-  var themeToggleBtn = document.getElementById("theme-toggle");
+  // var themeToggleBtn = document.getElementById("theme-toggle");
 
-  themeToggleBtn.addEventListener("click", function () {
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle("hidden");
-    themeToggleLightIcon.classList.toggle("hidden");
+  var themeToggleBtn = document.getElementsByClassName("theme-toggle");
 
-    // if set via local storage previously
-    if (localStorage.getItem("color-theme")) {
-      if (localStorage.getItem("color-theme") === "light") {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
+  for (let i = 0; i < themeToggleBtn.length; i++) {
+    themeToggleBtn[i].addEventListener("click", function () {
+      console.log("Change theme with: " + i + " button");
+      themeToggleDarkIcon[i].classList.toggle("hidden");
+      themeToggleLightIcon[i].classList.toggle("hidden");
+
+      // if set via local storage previously
+      if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+          document.documentElement.classList.add("dark");
+          localStorage.setItem("color-theme", "dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+          localStorage.setItem("color-theme", "light");
+        }
+
+        // if NOT set via local storage previously
       } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
+        if (document.documentElement.classList.contains("dark")) {
+          document.documentElement.classList.remove("dark");
+          localStorage.setItem("color-theme", "light");
+        } else {
+          document.documentElement.classList.add("dark");
+          localStorage.setItem("color-theme", "dark");
+        }
       }
-
-      // if NOT set via local storage previously
-    } else {
-      if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("color-theme", "light");
-      } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("color-theme", "dark");
-      }
-    }
-  });
+    });
+  }
 }
 
 function directScroll() {
