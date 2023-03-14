35cf31402544e345ed899386a40429f8fab22b52
fjelder <42210053+fjelder@users.noreply.github.com>
2023-03-14 14:43:52 +0100

    add swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 import Alpine from "alpinejs";
-
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import Swiper from "swiper";
+
+window.Swiper = Swiper;
 
 import { darkMode, checkDarkMode, directScroll } from "./test";
 
